88493080590f4d71a714b9363481bbd482d28885
David Le Jolly <zahara.alnour@gmail.com>
2022-01-28 15:55:44 +0300

    Trying to make mathlive work

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,121 @@
+<script>
+	import TopAppBar, { Row, Section, Title as TitleBar } from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+	import Drawer, { AppContent, Content, Header, Title, Subtitle, Scrim } from '@smui/drawer';
+	import Button, { Label } from '@smui/button';
+	import List, { Item, Text, Graphic, Separator, Subheader } from '@smui/list';
+	import { H6 } from '@smui/common/elements';
+
+	let active = 'Inbox';
+	let prominent = false;
+	let dense = false;
+	let secondaryColor = false;
+	let miniWindow = false;
+	let showDrawer = false;
+	const setMiniWindow = () => (miniWindow = window.innerWidth < 720);
+	const toggleDrawer = () => {
+		showDrawer = !showDrawer;
+	};
+
+	function setActive(value) {
+		active = value;
+		showDrawer = false;
+	}
+
+	$: console.log('drawer', showDrawer)
+</script>
+
+<svelte:window on:resize={setMiniWindow} />
+
+<Drawer variant="modal" bind:open={showDrawer}>
+	<Header>
+		<TitleBar>Super Mail</TitleBar>
+		<Subtitle>It's the best fake mail app drawer.</Subtitle>
+	</Header>
+	<Content>
+		<List>
+			<Item
+				href="javascript:void(0)"
+				on:click={() => setActive('Inbox')}
+				activated={active === 'Inbox'}
+			>
+				<Graphic class="material-icons" aria-hidden="true">inbox</Graphic>
+				<Text>Inbox</Text>
+			</Item>
+			<Item
+				href="javascript:void(0)"
+				on:click={() => setActive('Star')}
+				activated={active === 'Star'}
+			>
+				<Graphic class="material-icons" aria-hidden="true">star</Graphic>
+				<Text>Star</Text>
+			</Item>
+			<Item
+				href="javascript:void(0)"
+				on:click={() => setActive('Sent Mail')}
+				activated={active === 'Sent Mail'}
+			>
+				<Graphic class="material-icons" aria-hidden="true">send</Graphic>
+				<Text>Sent Mail</Text>
+			</Item>
+			<Item
+				href="javascript:void(0)"
+				on:click={() => setActive('Drafts')}
+				activated={active === 'Drafts'}
+			>
+				<Graphic class="material-icons" aria-hidden="true">drafts</Graphic>
+				<Text>Drafts</Text>
+			</Item>
+
+			<Separator />
+			<Subheader component={H6}>Labels</Subheader>
+			<Item
+				href="javascript:void(0)"
+				on:click={() => setActive('Family')}
+				activated={active === 'Family'}
+			>
+				<Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+				<Text>Family</Text>
+			</Item>
+			<Item
+				href="javascript:void(0)"
+				on:click={() => setActive('Friends')}
+				activated={active === 'Friends'}
+			>
+				<Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+				<Text>Friends</Text>
+			</Item>
+			<Item
+				href="javascript:void(0)"
+				on:click={() => setActive('Work')}
+				activated={active === 'Work'}
+			>
+				<Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+				<Text>Work</Text>
+			</Item>
+		</List>
+	</Content>
+</Drawer>
+
+<Scrim />
+<AppContent class="app-content">
+	<TopAppBar variant="static" {prominent} {dense} color={secondaryColor ? 'secondary' : 'primary'}>
+		<Row>
+			<Section>
+				<Title>Ubumaths</Title>
+			</Section>
+			<Section align="end" toolbar>
+				<IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+				<IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+				<IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+				{#if miniWindow}
+					<IconButton on:click={toggleDrawer} class="material-icons">menu</IconButton>
+				{/if}
+			</Section>
+		</Row>
+	</TopAppBar>
+	<slot />
+</AppContent>
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
+<script>
+	import Button, { Label } from '@smui/button';
+    import {convertLatexToMarkup} from 'mathlive/dist/mathlive.min.mjs'
+	// import {convertLatexToMarkup}
+	// from 'https://unpkg.com/mathlive/dist/mathlive.min.mjs';
+
+	// let convertLatexToMarkup
+    // let markup = ''
+    // import('https://unpkg.com/mathlive/dist/mathlive.min.mjs')
+    // .then(m => {convertLatexToMarkup= m.convertLatexToMarkup})
+    // .catch(e=> {console.log('erreur', e)})
+        
+    // $:if (convertLatexToMarkup) {
+    //     markup = convertLatexToMarkup('\\frac{3}{4}')
+    // }
+    let markup=convertLatexToMarkup('\\frac{3}{4}')
+
+  
+    // let markup = convertLatexToMarkup('\\frac{3}{4}');
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button>
+	<Label>Default</Label>
+</Button>
+{@html markup}
